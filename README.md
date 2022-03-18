40db0f77744664322bd60a71c2bcb543d6076bd5
Dados Abertos Sergipe <adolfoguimaraes@gmail.com>
2022-03-18 16:26:39 -0300

    Atualização prolog

--- README.md
+++ README.md
@@ -7,5 +7,7 @@
  1. [Algoritmos de Busca](code/01_AlgoritmosDeBusca.ipynb)
  2. [Subida de Encosta](code/02_SubidaDeEncosta.ipynb)
  3. [Algoritmo MinMax](code/03_Algoritmo_MinMax.ipynb)
+ 4. [Prolog](code/04_Prolog.ipynb)
+
 
  
---
+++ datasets/kb_prolog/base1.pl
@@ -0,0 +1,24 @@
+homem(rickard).
+homem(ned).
+homem(robb).
+homem(bran).
+homem(rickon).
+homem(jon).
+mulher(sansa).
+mulher(arya).
+mulher(catelyn).
+
+progenitor(rickard, ned).
+progenitor(ned, robb).
+progenitor(ned, sansa).
+progenitor(ned, arya).
+progenitor(ned, bran).
+progenitor(ned, rickon).
+progenitor(ned, jon).
+progenitor(catelyn, robb).
+progenitor(catelyn, sansa).
+progenitor(catelyn, arya).
+progenitor(catelyn, bran).
+progenitor(catelyn, rickon).
+
+casados(ned, catelyn).

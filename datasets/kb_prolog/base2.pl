58003bf7158363f4354a30e0de6cd6c2539bad2d
Dados Abertos Sergipe <adolfoguimaraes@gmail.com>
2022-03-18 17:12:05 -0300

    Adicionado o arquivo base2.pl

---
+++ datasets/kb_prolog/base2.pl
@@ -0,0 +1,34 @@
+homem(gustavo).
+homem(americo).
+homem(daniel).
+homem(paulo).
+homem(carlos).
+homem(joaquim).
+homem(filipe).
+
+mulher(teresa).
+mulher(sonia).
+mulher(carla).
+mulher(barbara).
+mulher(maria).
+mulher(joana).
+mulher(ana).
+
+pai(gustavo, joaquim).
+pai(carlos, teresa).
+pai(daniel, americo).
+pai(daniel, paulo).
+pai(joaquim, daniel).
+pai(joaquim, barbara).
+pai(americo, ana).
+
+mae(maria, daniel).
+mae(maria, barbara).
+mae(barbara, joana).
+mae(teresa, ana).
+mae(joana, filipe).
+
+casados(filipe, carla).
+casados(americo, teresa).
+casados(carlos, sonia).
+casados(joaquim, maria).
